edb205fdd48d4520ada38b9a2e1cfe445df2e045
Daniel Sedlak <daniel.sedlak@bigclown.com>
2017-08-17 12:15:25 +0200

    Fix starting errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+const path = require("path");
+
 module.exports = {
-    entry: "src/index.js",
+    entry: "./src/index.js",
     output: {
-        filename: "view/bundle.js"
+        path: path.join(__dirname,"./view"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            { test: /\.js?$/, loader: "babel-loader" ,exclude: /node_modules/ }
+        ]
+    },
+    devServer: {
+        port: 4000,
+        contentBase: "./view",
+        historyApiFallback: false
     }
 }
